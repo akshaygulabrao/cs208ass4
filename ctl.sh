75b269fcd2c7e07be1baac5e5a741966545933ac
Akshay Gulabrao <32472626+actionpackedgeneral@users.noreply.github.com>
2022-02-11 07:45:01 -0800

    Update ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -3,6 +3,7 @@ openstack server list > server_list.txt
 mid=`awk 'NR == 4 {print $2}' server_list.txt`
 openstack server show $mid > server_show.txt
 from_host=`awk '/OS-EXT-SRV-ATTR:host/ {print $4}' server_show.txt`
+openstack compute service list > service_list.txt
 compute_number=${from_host:3:1}
 if [ "$compute_number" = "1" ];
 then
@@ -10,7 +11,7 @@ then
 else
         to_host=`awk '/cp-1/ {print $6}' service_list.txt`
 fi
-echo $mid
-echo $from_host
-echo $to_host
+echo "Migration ID:"$mid
+echo "From Host:"$from_host
+echo "To Host"$to_host
 time openstack server migrate $mid --live $to_host --block-migration
